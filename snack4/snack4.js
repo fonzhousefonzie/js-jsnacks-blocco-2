20292309baaad4cbc8a2cc42141c5700ad18d035
fonzhousefonzie <harth.fonz@gmail.com>
2022-05-24 15:30:08 +0200

    snack4(?)

---
+++ snack4/snack4.js
@@ -0,0 +1,29 @@
+const arrayUno = [];
+const arrayDue = [];
+
+
+for (let i = 0; i < Math.floor(Math.random()*99); i++){
+    let randomNum = Math.floor(Math.random())*99;
+    arrayUno.push(randomNum);
+}
+
+for (let i = 0; i < (Math.floor(Math.random()*99)); i++){
+    let randomNum = Math.floor(Math.random())*99;
+    arrayDue.push(randomNum);
+}
+
+console.log(arrayUno);
+console.log(arrayDue);
+
+if(arrayUno.length < arrayDue.length){
+    for (let i = arrayUno.length; i<arrayDue.length; i++){
+        arrayUno.push(arrayUno.length);
+    }
+} else {
+    for (let i = arrayDue.length; i<arrayUno.length; i++){
+        arrayUno.push(arrayDue.length);
+    }
+}
+
+console.log(arrayUno);
+console.log(arrayDue);
